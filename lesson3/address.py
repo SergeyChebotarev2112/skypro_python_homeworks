e15242f9f9331fd3f51ea8ecd4120fb5a8db3e8b
SergeyChebotarev2112 <160312028+SergeyChebotarev2112@users.noreply.github.com>
2024-07-30 07:31:52 +0700

    Update address.py
    Убрал создание объекта и вызов методов.

--- lesson3/address.py
+++ lesson3/address.py
@@ -5,12 +5,3 @@ class Address:
   self.street = street
   self.house = house
   self.apartment = apartment
-
-  # Пример создания объекта класса Address
-my_address = Address("111111", "Владивосток", "Снеговая улица", "5", "1")
-
-print(my_address.index)
-print(my_address.city)
-print(my_address.street)
-print(my_address.house)
-print(my_address.apartment)
