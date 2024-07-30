a27aeacd4b40a07a8326533a5065801b5681d3d7
SergeyChebotarev2112 <160312028+SergeyChebotarev2112@users.noreply.github.com>
2024-07-30 08:02:12 +0700

    Update lesson_3_task_2.py
    1. Добавил импорт класса
    2. Убрал создание объекта

--- lesson3/lesson_3_task_2.py
+++ lesson3/lesson_3_task_2.py
@@ -1,8 +1,4 @@
-class Smartphone:
- def __init__(self, phone_brand, phone_model, phone_number):
-  self.phone_brand = phone_brand
-  self.phone_model = phone_model
-  self.phone_number = phone_number
+from smartphone import Smartphone
 
 # Создание списка из пяти объектов Smartphone
 catalog = [
