366f490610c4e79372141e13e7187394eb4d0705
SergeyChebotarev2112 <160312028+SergeyChebotarev2112@users.noreply.github.com>
2024-07-30 08:11:40 +0700

    Update smartphone.py
    Убрал создание объекта

--- lesson3/smartphone.py
+++ lesson3/smartphone.py
@@ -3,11 +3,3 @@ class Smartphone:
   self.phone_brand = phone_brand
   self.phone_model = phone_model
   self.phone_number = phone_number
-
-
-my_smartphone = Smartphone("Apple", "iPhone 13 Pro", "+79123456789")
-
-
-print(my_smartphone.phone_brand) 
-print(my_smartphone.phone_model) 
-print(my_smartphone.phone_number) 
